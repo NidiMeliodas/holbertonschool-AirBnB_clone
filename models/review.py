e6279ac697e04332908318f71b2c3c7573f4fb49
NidiMeliodas <nidigashan@gmail.com>
2024-03-08 17:22:51 +0100

    unitest

--- models/review.py
+++ models/review.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""
+Module for the Review class.
+"""
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """Represents a review."""
+
+    def __init__(self, *args, **kwargs):
+        """Initialize Review instance."""
+        super().__init__(*args, **kwargs)
+        self.place_id = kwargs.get('place_id', "")
+        self.user_id = kwargs.get('user_id', "")
+        self.text = kwargs.get('text', "")
