06d56009f38aaf37328e933d490cc1a4777985d5
NidiMeliodas <nidigashan@gmail.com>
2024-03-08 23:52:05 +0100

    console fix ?

--- console.py
+++ console.py
@@ -9,23 +9,17 @@ class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
 
     def do_quit(self, arg):
-        """Exit the program."""
+        """Exit the program"""
         return True
 
     def do_EOF(self, arg):
-        """Exit the program."""
+        """Exit the program"""
         print()
         return True
 
     def emptyline(self):
-        """Do nothing on empty line."""
+        """Do nothing on empty line"""
         pass
-
-    def default(self, line):
-        """Handle invalid commands."""
-        if not line:
-            return
-        print("*** Unknown syntax: {}".format(line))
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
